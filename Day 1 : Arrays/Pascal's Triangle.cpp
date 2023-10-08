b126f00147f1052c073adb9626d61a5259ddbbac
Mahipal Suchar <104795672+its-mahi@users.noreply.github.com>
2023-10-08 23:56:39 +0530

    Update Pascal's Triangle.cpp

--- Day 1 : Arrays/Pascal's Triangle.cpp
+++ Day 1 : Arrays/Pascal's Triangle.cpp
@@ -16,24 +16,34 @@
  *      
  *      Explanation: The output matrix has the first four rows of Pascal's Triangle.
  * 
+ * Time Complexity : O(N^2)
+ * Space Complexity : O(N^2)
+ *
  * Difficulty: Easy
 */
 
 vector<vector<int>> pascalTriangle(int N) {
 
     vector<vector<int> > ans;
+    // initialize the first row of Pascal triangle
     ans.push_back({1});
 
     for(int i=2 ; i<=N ; i++) {
+        // ith row of Pascal triangle
         vector<int> temp;
+
+        // first element will always 1
         temp.push_back(1);
 
         for(int j=1 ; j<i-1 ; j++) {
+            // sum of (j-1)th and jth of previous row
             temp.push_back(ans[i-2][j-1] + ans[i-2][j]);
         }
 
+        // last element will always be 1
         temp.push_back(1);
 
+        // pushing ith row to Pascal triangle
         ans.push_back(temp);
     }
 
